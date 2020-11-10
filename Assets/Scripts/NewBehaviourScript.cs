17bb03138f4b42163d2fde35cd3948b5dd35ff97
OlhaKhytra <olya.khitra@gmail.com>
2020-11-10 12:50:45 +0100

    with enemy

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public Transform player;
+    public float move_speed;
+    public float rotation_speed;
+    public Transform enemy;
+    void Update()
+    {
+        var look_dir = player.position - enemy.position;
+        look_dir.y = 0;
+        enemy.rotation = Quaternion.Slerp(enemy.rotation, Quaternion.LookRotation(look_dir), rotation_speed * Time.deltaTime);
+        enemy.position += enemy.forward * move_speed * Time.deltaTime;
+    }
+}
+
