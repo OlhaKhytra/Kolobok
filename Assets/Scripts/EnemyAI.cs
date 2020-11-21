9d31f44956124bdb6f5e572baf1100dd45abcd4e
alexnoobmaster <71437831+alexnoobmaster@users.noreply.github.com>
2020-11-21 16:22:39 +0100

    Alex

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+
+
+    Transform playerTransform;
+    UnityEngine.AI.NavMeshAgent myNavmesh;
+    public float checkRate = 0.01f;
+    float nextCheck;
+
+    void Start ()
+    {
+        if (GameObject.FindGameObjectWithTag("Kolobok").activeInHierarchy)
+            playerTransform = GameObject.FindGameObjectWithTag("Kolobok").transform;
+
+        myNavmesh = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if(Time.time > nextCheck)
+        {
+            nextCheck = Time.time + checkRate;
+            FollowPlayer();
+        }
+    }
+
+    void FollowPlayer()
+    {
+        myNavmesh.transform.LookAt(playerTransform);
+        myNavmesh.destination = playerTransform.position;
+    }
+}
---
+++ Assets/Scripts/barrier.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class barrier : MonoBehaviour
+{
+    public float speed;
+    public Transform _transformObj;
+    private Vector3 direction;
+
+    private void Start()
+    {
+        _transformObj = GetComponent<Transform>();
+        direction = Vector3.forward;
+    }
+
+     private void Update()
+    {
+        _transformObj.Translate(direction * speed * Time.deltaTime);
+
+        if(_transformObj.position.z > 3)
+        {
+            direction = -Vector3.forward;
+        }
+
+        if (_transformObj.position.z > 3)
+        {
+            direction = Vector3.forward;
+        }
+    }
+
+}
