e0286cfcac0ea71c98ea17f6cd71fe98ac59002f
OlhaKhytra <olya.khitra@gmail.com>
2020-12-08 13:58:36 +0100

    Upgrate

---
+++ Assets/Scripts/RotationTRap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationTRap : MonoBehaviour
+{
+    public float rotationspeed;
+    void Start()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        this.transform.Rotate(new Vector3(0, 0, rotationspeed));
+    }
+   
+}
